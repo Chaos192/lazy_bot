346a1caf1b020c4f997eff7609c787832cc7f3cd
Felipe David <felipedavid.huh@gmail.com>
2021-04-23 20:35:18 -0300

    .

--- bot/build.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-cl.exe /D_USRDLL /D_WINDLL main.c lazybot.c object_manager.c user32.lib /link /DLL /OUT:lazybot.dll
--- injector/build.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-cl.exe injector.c User32.lib Shell32.lib Advapi32.lib /link /OUT:injector.exe
